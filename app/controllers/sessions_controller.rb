4f32b1b0748cea109bcf52f9b2333d32c413a758
bakitheme <gersi00057@gmail.com>
2020-07-11 15:04:04 +0300

    login form added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      # redirect to tickets menaging page
+    else
+      flash.now[:danger] = 'Email or password error'
+      render 'new'
   end
 end

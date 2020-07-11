f52b31bea837fcd70e954604d54cd2d1fa2affb2
bakitheme <gersi00057@gmail.com>
2020-07-11 19:54:50 +0300

    log in & log out completed

--- Gemfile
+++ Gemfile
@@ -4,12 +4,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.3'
 
 gem 'rails', '~> 5.2.4', '>= 5.2.4.3'
+gem 'bcrypt'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 
-gem 'bcrypt'
 gem 'dotenv-rails'
 gem 'rubocop'
 gem 'haml-rails'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,18 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
-      # redirect to tickets menaging page
+      log_in user
+      # redirect_to tickets
+      # change to line above when ticket finished
+      redirect_to root_url
     else
       flash.now[:danger] = 'Email or password error'
       render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
   end
 end

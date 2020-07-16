0a7a75c9dc8c41d4cbf7a12df90dc8648ba28657
bakitheme <gersi00057@gmail.com>
2020-07-16 21:01:16 +0300

    ticket update added

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -13,6 +13,12 @@ class TicketsController < ApplicationController
 
     if @ticket.save
       @ticket.send_activation_email
+
+      History.create!(header: 'This ticket created',
+                      content: "Ticket author: #{ticket.client_name} topic
+                                of this ticket - #{ticket.subject}",
+                      ticket_id: @ticket.id )
+
       flash[:success] = 'Check your e-mail for ticket activation'
       redirect_to root_url
     else
@@ -29,7 +35,29 @@ class TicketsController < ApplicationController
   end
 
   def edit
+    @ticket = Ticket.find(params[:id])
+  end
 
+  def update
+    @ticket = Ticket.find(params[:id])
+
+    if @ticket.update_attributes(ticket_params)
+      flash[:success] = 'Successfully changed :)'
+
+      if logged_in?
+        History.create!(header: "#{@current_user.name} | updated ticket",
+                        content: "Ticket status now is:
+                                  #{@ticket.ticket_status.status_name}",
+                        ticket_id: @ticket.id )
+      else
+        History.create!(header: "#{@ticket.client_name} | updated ticket",
+                        content: @ticket.content,
+                        ticket_id: @ticket.id )
+      end
+      redirect_to request.referrer
+    else
+      render 'edit'
+    end
   end
 
   private
